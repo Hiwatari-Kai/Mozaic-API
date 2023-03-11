c3284451f9a25c90d8b2ffed42b10826b8e17c61
Manish Kumar Das <ammanishkumardas@gmail.com>
2023-03-12 02:06:45 +0530

    message on connection to db

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 // mongoose.set("strictQuery", true);
 
-mongoose.connect(process.env.MONGODB_URL, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-});
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('connected to database...')
+  })
